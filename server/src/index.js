a6e7d9d8469e47ebba9b19795c7d3d64b423fe60
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2025-01-03 22:08:52 +0200

    delete server

--- server/src/index.js
+++
@@ -1,9 +0,0 @@
-import { startServer } from './server.js';
-import { initMongoDB } from './db/initMongoDB.js';
-
-const bootstrap = async () => {
-  await initMongoDB();
-  startServer();
-};
-
-bootstrap();
